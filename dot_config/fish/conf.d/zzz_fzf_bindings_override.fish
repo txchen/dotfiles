72572ae11786d1ba04e447ca0fcd746893b5c565
Tianxiang Chen <txchendev@gmail.com>
2026-03-04 22:14:38 -0800

    update fish

--- dot_config/fish/conf.d/zzz_fzf_bindings_override.fish
+++ dot_config/fish/conf.d/zzz_fzf_bindings_override.fish
@@ -1,5 +1,11 @@
 # Override fzf.fish key bindings after plugin init.
-# Keep git/variables bindings disabled, but enable directory search shortcut.
+# Keep git/variables bindings disabled, but keep directory search shortcut.
 if functions -q fzf_configure_bindings
     fzf_configure_bindings --git_log= --git_status= --variables=
+end
+
+# Enforce Ctrl+Alt+F for directory search even if plugin defaults change.
+if functions -q _fzf_search_directory
+    bind ctrl-alt-f _fzf_search_directory
+    bind -M insert ctrl-alt-f _fzf_search_directory
 end
