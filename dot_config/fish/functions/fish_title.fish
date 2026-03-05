5711049485f338963ebdd8985951d2b1955ff308
Tianxiang Chen <txchendev@gmail.com>
2026-03-04 21:52:33 -0800

    update

--- dot_config/fish/functions/fish_title.fish
+++ dot_config/fish/functions/fish_title.fish
@@ -1,9 +1,10 @@
 function fish_title
     set -l host (prompt_hostname)
+    set -l dir (prompt_pwd)
 
     if set -q SSH_TTY
-        echo "[$host]"
+        echo "[$host] $dir"
     else
-        echo "$host "(prompt_pwd)
+        echo "$host $dir"
     end
 end
